c8e86744ed85048eb9100d962aba09e624a385e7
ifan sultoni <ifansmiling66@gmail.com>
2024-12-26 13:51:50 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TestimonialController;
 use App\Http\Controllers\PublicTestimonialController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TransactionController;
 
 
 Route::get('/', [ProductController::class, 'showProducts'])->name('home');
@@ -16,8 +15,6 @@ Route::get('/testimonials/create', [TestimonialController::class, 'create'])->na
 Route::post('/testimonials', [TestimonialController::class, 'store'])->name('testimonials.store');
 Route::post('/testimonials', [PublicTestimonialController::class, 'store'])->name('testimonials.store');
 Route::get('/testimonials', [TestimonialController::class, 'userIndex'])->name('testimonials.index');
-Route::post('/checkout', [TransactionController::class, 'store'])->name('transactions.store');
-Route::get('/transactions/{id}', [TransactionController::class, 'show'])->name('transactions.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
